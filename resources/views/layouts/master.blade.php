6711e004913cf98797c8caa84cf2e412bfb87d55
Dawud Mauludi <dawudmauludi15@gmail.com>
2025-05-22 14:32:43 +0700

    approve Update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,13 +7,20 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<<<<<<< Updated upstream
     <script src="https://unpkg.com/alpinejs" defer></script>
     <script src="https://unpkg.com/feather-icons"></script>
+=======
+>>>>>>> Stashed changes
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<<<<<<< Updated upstream
 
+=======
+    
+>>>>>>> Stashed changes
     <script src="https://cdn.tailwindcss.com"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
@@ -35,11 +42,21 @@
     @yield('styles')
 </head>
 <body class="flex flex-col min-h-screen font-sans">
+
+  
+
     @include('partials.navbar')
     <!-- Main Content -->
      <main class="flex-grow bg-gray-100">
         @yield('content')
     </main>
+
+
+
+    
+    @include('partials.footer')
+    <!-- Footer -->
+  
 
 
     <a href="https://wa.me/6288228513539" target="_blank"
