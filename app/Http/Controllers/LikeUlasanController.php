c221e97d6611f18ea983438ff5fd86fb7603a3a4
Dawud Mauludi <dawudmauludi15@gmail.com>
2025-06-04 21:36:19 +0700

    setup database alumni

---
+++ app/Http/Controllers/LikeUlasanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\like_ulasan;
+use Illuminate\Http\Request;
+
+class LikeUlasanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(like_ulasan $like_ulasan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(like_ulasan $like_ulasan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, like_ulasan $like_ulasan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(like_ulasan $like_ulasan)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UlasanAlumniController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ulasan_alumni;
+use Illuminate\Http\Request;
+
+class UlasanAlumniController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ulasan_alumni $ulasan_alumni)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ulasan_alumni $ulasan_alumni)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ulasan_alumni $ulasan_alumni)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ulasan_alumni $ulasan_alumni)
+    {
+        //
+    }
+}
